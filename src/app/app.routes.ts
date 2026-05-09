13480b88132b6e1de21db0cf5e1a277a84b1efd2
Stefano N <30480337+snebo@users.noreply.github.com>
2026-05-09 17:46:45 +0100

    style: add style changes for main home page
    Signed-off-by: Stefano N <30480337+snebo@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,49 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 import { guestGuard } from './core/guards/guest.guard';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent),
-    canActivate: [guestGuard]
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+    canActivate: [guestGuard],
   },
   {
     path: 'signup',
-    loadComponent: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent),
-    canActivate: [guestGuard]
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'poll/:id',
-    loadComponent: () => import('./features/poll-details/poll-details').then(m => m.PollDetails),
-    canActivate: [authGuard]
+    loadComponent: () =>
+      import('./features/auth/signup/signup.component').then((m) => m.SignupComponent),
+    canActivate: [guestGuard],
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'poll/:id',
+        loadComponent: () =>
+          import('./features/poll-details/poll-details').then((m) => m.PollDetails),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
   },
   {
     path: '**',
-    redirectTo: 'home'
-  }
+    redirectTo: '404',
+  },
 ];
